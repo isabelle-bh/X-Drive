c1157fb55303fadf38a098b883cd4727ffa6d71a
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2026-04-20 16:07:48 -0400

    feat: session creation on login

--- x-drive-crm/app/api/auth/login/route.ts
+++ x-drive-crm/app/api/auth/login/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 import bcrypt from "bcrypt"
+import { cookies } from "next/headers"
+import { randomUUID } from "crypto"
 
 export async function POST(req: Request) {
     const { email, password } = await req.json();
@@ -11,15 +13,43 @@ export async function POST(req: Request) {
         }
     });
 
-    if (!user || !user.password) {
-        return new NextResponse("Invalid credentials", { status: 401 });
+    if (!user) {
+        return new NextResponse(JSON.stringify({message: "Invalid user"}), { status: 400 });
+    }
+
+    if (!user.password) {
+        (await cookies()).set("temp-email", email, {
+            httpOnly: true,
+            secure: true,
+            path: "/",
+            maxAge: 60 * 10 // 10 mins
+        })
+        
+        return new NextResponse(JSON.stringify({message: "Password not set yet"}), {status: 400});
     }
 
     const valid = await bcrypt.compare(password, user.password);
 
     if (!valid) {
-        return new NextResponse("Invalid password", { status: 401 })
+        return new NextResponse(JSON.stringify({message: "Invalid password"}), { status: 401 })
     }
+
+    const sessionId = randomUUID();
+
+    await prisma.session.create({
+        data: {
+            id: sessionId,
+            userId: user.id,
+            expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7) // 7 days
+        }
+    });
+
+    (await cookies()).set("session", sessionId, {
+        httpOnly: true,
+        secure: true,
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7
+    })
 
     return NextResponse.json({ userId: user.id});
 }
