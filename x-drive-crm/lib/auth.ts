b0a53b640857eeff22c5710059b9a407bc41d751
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2026-04-20 16:05:57 -0400

    feat: helper function for session identification

--- x-drive-crm/lib/auth.ts
+++ x-drive-crm/lib/auth.ts
@@ -1,7 +1,26 @@
+import { prisma } from "@/lib/prisma"
+import { cookies } from "next/headers"
+
+export async function getCurrentUser() {
+    const sessionId = (await cookies()).get("session")?.value;
+
+    const session = await prisma.session.findUnique({
+        where: { id: sessionId },
+        include: { user: true }
+    })
+
+    if (!session) {
+        return null
+    }
+
+    return session.user
+}
 
 
 export function newPasswordValid(pwd: string) {
     if (pwd.length < 8) {
-        return "Error: Password must be at least 8 characters long!";
+        return false;
+    } else {
+        return true;
     }
 }
