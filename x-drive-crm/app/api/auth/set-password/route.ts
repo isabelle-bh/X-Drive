91ee82a0b95ef837d25bb00877ea1331696119b0
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2026-04-20 16:08:22 -0400

    feat: set password functionality working

--- x-drive-crm/app/api/auth/set-password/route.ts
+++ x-drive-crm/app/api/auth/set-password/route.ts
@@ -1,11 +1,21 @@
 import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 import bcrypt from "bcrypt"
+import { newPasswordValid } from "@/lib/auth"
+import { cookies } from "next/headers"
 
 // setting password
 export async function POST(req: Request) {
+    const email = (await cookies()).get("temp-email")?.value;
+    if (!email) {
+        return new NextResponse(JSON.stringify({message: "Session expired. Please attempt log in again.", redirect: "/auth/login" }), {status: 401},);
+    }
 
-    const {email, password} = await req.json();
+    const {password} = await req.json();
+
+    if (!newPasswordValid(password)) {
+        return new NextResponse(JSON.stringify({message: "Password must be at least 8 characters long." }), {status: 400});
+    }
     
     const hashed = await bcrypt.hash(password, 10);
 
